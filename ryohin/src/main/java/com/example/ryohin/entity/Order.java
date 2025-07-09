641c59e1848f3d116a1f9016de69c5d83aafa79e
ainaga09 <ai.nagaba@avantcorp.com>
2025-07-09 17:05:50 +0900

    Order更新

--- ryohin/src/main/java/com/example/ryohin/entity/Order.java
+++ ryohin/src/main/java/com/example/ryohin/entity/Order.java
@@ -42,7 +42,7 @@ public class Order {
     private String order_status;
     
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Orderitem> orderDetails = new ArrayList<>();
+    private List<OrderItem> orderDetails = new ArrayList<>();
     
     private LocalDateTime createdAt;
     
@@ -60,8 +60,8 @@ public class Order {
     }
     
     // Helper method to add order detail
-    public void addOrderDetail(Orderitem orderDetail) {
-        orderitem.add(orderitem);
-        orderitem.setOrder(this);
+    public void addOrderItem(OrderItem orderItem) {
+        OrderItem.add(orderItem);
+        OrderItem.setOrder(this);
     }
 }
