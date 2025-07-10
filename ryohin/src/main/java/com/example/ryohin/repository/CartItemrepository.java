55a46cba232ead4f73bc2bba12f3030e10cae201
takuma.hino <takuma.hino@avantcorp.com>
2025-07-10 13:57:09 +0900

    a

--- ryohin/src/main/java/com/example/ryohin/repository/CartItemrepository.java
+++ ryohin/src/main/java/com/example/ryohin/repository/CartItemrepository.java
@@ -1,11 +1,11 @@
 package com.example.ryohin.repository;
 
-import com.example.ryohin.entity.Cart_Item;
+import com.example.ryohin.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartItemrepository extends JpaRepository<Cart_Item, Integer> {
+public interface CartItemrepository extends JpaRepository<CartItem, Integer> {
 
 } 
 
