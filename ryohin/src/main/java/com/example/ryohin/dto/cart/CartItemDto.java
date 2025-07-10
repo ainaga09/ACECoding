9c9e2d0f3be3ff89cf734e2f35fde921fccf8d40
takuma.hino <takuma.hino@avantcorp.com>
2025-07-10 10:45:45 +0900

    itemdto

--- ryohin/src/main/java/com/example/ryohin/dto/cart/CartItemDto.java
+++ ryohin/src/main/java/com/example/ryohin/dto/cart/CartItemDto.java
@@ -1,5 +1,21 @@
 package com.example.ryohin.dto.cart;
 
-public class CartItemDto {
-    
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class CartItemDto implements Serializable {
+
+    private String productId;
+    private String name;
+    private BigDecimal price;
+    private int quantity;
+    // getters, setters, constructor
 }
