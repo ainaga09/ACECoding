279e8aaf2847172f9a2e22002e568edba4d3afca
miu-hirota <miu.hirota@avantcorp.com>
2025-07-10 10:57:03 +0900

    ProductDetailDTO

--- ryohin/src/main/java/com/example/ryohin/dto/product/ProductDetail.java
+++ ryohin/src/main/java/com/example/ryohin/dto/product/ProductDetail.java
@@ -1,5 +1,23 @@
 package com.example.ryohin.dto.product;
 
+import jakarta.websocket.Decoder.Text;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDetail {
-    
+    private Integer product_Id;
+    private String product_name;
+    private String description;
+    private String material;
+    private BigDecimal price;
+    private Integer stock_quantity;
+    private String image_url;
+    private LocalDateTime created_At;
+    private LocalDateTime updated_at;
 }
