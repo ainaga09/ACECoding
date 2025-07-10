43d8108fc7447dfd12057cb4e7d061f78f7ca08f
T-Nagara <takuma.nagara@avantcorp.com>
2025-07-10 11:25:48 +0900

    Loginrequest

--- ryohin/src/main/java/com/example/ryohin/dto/customer/loginRequest.java
+++ ryohin/src/main/java/com/example/ryohin/dto/customer/loginRequest.java
@@ -1,5 +1,22 @@
 package com.example.ryohin.dto.customer;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class loginRequest {
-    
+
+ @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
 }
