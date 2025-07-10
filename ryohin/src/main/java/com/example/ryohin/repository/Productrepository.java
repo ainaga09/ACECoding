0d71e53c262461e91c5b38e80a59fa4a7e1f6090
miu-hirota <miu.hirota@avantcorp.com>
2025-07-10 10:19:57 +0900

    Productリポジトリ

--- ryohin/src/main/java/com/example/ryohin/repository/Productrepository.java
+++ ryohin/src/main/java/com/example/ryohin/repository/Productrepository.java
@@ -1,5 +1,15 @@
 package com.example.ryohin.repository;
 
-public class Productrepository {
+import com.example.ryohin.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
     
+    @Modifying
+    @Query("UPDATE Product p SET p.stock = p.stock - ?2 WHERE p.productId = ?1 AND p.stock >= ?2")
+    int decreaseStock(Integer productId, Integer quantity);
 }
