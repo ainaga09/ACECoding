94bf62a56181ce7c8091b9914a41074b2eaa0128
ainaga09 <ai.nagaba@avantcorp.com>
2025-07-10 10:38:00 +0900

    orderResponse更新

--- ryohin/src/main/java/com/example/ryohin/dto/order/orderResponse.java
+++ ryohin/src/main/java/com/example/ryohin/dto/order/orderResponse.java
@@ -1,5 +1,15 @@
 package com.example.ryohin.dto.order;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class orderResponse {
-    
+    private Integer order_id;
+    private LocalDateTime order_date;
 }
