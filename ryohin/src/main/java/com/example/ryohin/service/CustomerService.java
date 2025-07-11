789f6381e127084999098ea479cdcdf871644143
ainaga09 <ai.nagaba@avantcorp.com>
2025-07-11 12:32:18 +0900

    customercontroller更新

--- ryohin/src/main/java/com/example/ryohin/service/CustomerService.java
+++ ryohin/src/main/java/com/example/ryohin/service/CustomerService.java
@@ -1,5 +1,22 @@
-package com.example.ryohin.survice;
+package com.example.ryohin.service;
+
+import com.example.ryohin.dto.customer.CustomerRequest;
+import com.example.ryohin.dto.customer.Customerresponse;
+
+import jakarta.validation.Valid;
 
 public class CustomerService {
+
+    public Customerresponse getMockCustomer() {
+        throw new UnsupportedOperationException("Unimplemented method 'getMockCustomer'");
+    }
+
+    public Customerresponse registerCustomer(CustomerRequest customerRequest) {
+        throw new UnsupportedOperationException("Unimplemented method 'registerCustomer'");
+    }
+
+    public Customerresponse updateCustomerInfo(CustomerRequest customerRequest) {
+        throw new UnsupportedOperationException("Unimplemented method 'updateCustomerInfo'");
+    }
     
 }
