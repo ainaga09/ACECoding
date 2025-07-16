b718c2a67da8a83104ddb2c2dbf8061e837fe2d3
kohei.shimizu <kohei.shimizu@avantcorp.com>
2025-07-16 16:50:30 +0900

    テストファイルの追加

---
+++ ryohin/src/test/java/com/example/ryohin/controller/OrderControllertest.java
@@ -0,0 +1,5 @@
+package com.example.ryohin.controller;
+
+public class OrderControllertest {
+    
+}
---
+++ ryohin/src/test/java/com/example/ryohin/service/OrderServiceTest.java
@@ -0,0 +1,5 @@
+package com.example.ryohin.service;
+
+public class OrderServiceTest {
+    
+}
